5cd9af0eee9a42a552c8550547c2a758b2dfc351
Yin Wai <you@example.com>
2019-05-02 10:22:20 +0630

    Initial commit

---
+++ app/src/main/java/com/example/yinwainaing/ayaqrscan/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.yinwainaing.ayaqrscan;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new IntentIntegrator(this).initiateScan();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        IntentResult result=IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/yinwainaing/ayaqrscan/RetrofitAPI.java
@@ -0,0 +1,5 @@
+package com.example.yinwainaing.ayaqrscan;
+
+public interface RetrofitAPI {
+
+}
